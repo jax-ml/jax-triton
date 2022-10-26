6a628ebc99682664a962383eeba01c5a68aa3e12
Sharad Vikram <sharadmv@google.com>
2022-10-25 22:10:42 -0700

    Fix quotes in triton_call_test
    PiperOrigin-RevId: 483865328

--- tests/triton_call_test.py
+++ tests/triton_call_test.py
@@ -215,6 +215,6 @@ class TritonKernelCallTest(parameterized.TestCase):
     np.testing.assert_allclose(out, expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.5"
   absltest.main()
