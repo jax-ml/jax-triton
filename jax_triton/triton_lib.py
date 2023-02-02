1854041e951182d2af8fc85e088b7ea687b24c71
Chris Jones <cjfj@google.com>
2023-02-02 10:34:36 -0800

    Fix some lint errors.
    - Remove unused imports.
    - Reorder imports.
    - Consistently use double quotes.
    - Whitespace.
    
    PiperOrigin-RevId: 506662250

--- jax_triton/triton_lib.py
+++ jax_triton/triton_lib.py
@@ -14,28 +14,28 @@
 
 """Module for calling Triton kernels from JAX."""
 import collections
-import os
 import functools
 import math
+import os
 import pickle
 
 from typing import Any, Callable, Dict, Optional, Protocol, Sequence, Tuple, Union
 
 import jax
-import jax.dlpack
-import jax.numpy as jnp
-from jax.lib import xla_client as xc
-from jax.interpreters import mlir
-from jax.interpreters import xla
+import jaxlib
 from jax import tree_util
 from jax._src import core
 from jax._src import state
 from jax._src import util
-from jax._src.lib import xla_bridge as xb
-from jax._src.typing import Array
 from jax._src.lib.mlir import ir
 from jax._src.lib.mlir.dialects import mhlo
-import jaxlib
+from jax._src.typing import Array
+import jax.dlpack
+from jax.interpreters import mlir
+from jax.interpreters import xla
+from jax.lib import xla_client as xc
+import jax.numpy as jnp
+from jax_triton import triton_kernel_call_lib
 import numpy as np
 
 CAN_USE_TRITON = False
@@ -47,11 +47,10 @@ try:
 except ModuleNotFoundError:
   pass
 
-from jax_triton import triton_kernel_call_lib
-
 os.environ["TRITON_CACHE_DIR"] = ""
 map, unsafe_map = util.safe_map, map
 zip, unsafe_zip = util.safe_zip, zip
+
 
 def get_triton_type(obj: Any) -> str:
   type_map = {
@@ -81,22 +80,25 @@ def get_triton_type(obj: Any) -> str:
     obj = obj.value
   if isinstance(obj, int):
     if -2**31 <= obj < 2**31:
-      return 'i32'
+      return "i32"
     elif 2**31 <= obj < 2**32:
-      return 'u32'
+      return "u32"
     elif -2**63 <= obj < 2**63:
-      return 'i64'
+      return "i64"
     elif 2**63 <= obj < 2**64:
-      return 'u64'
+      return "u64"
     else:
-      raise ValueError(f'integer overflow representing {obj}')
+      raise ValueError(f"integer overflow representing {obj}")
   if isinstance(obj, float):
-    return 'f'
+    return "f"
   if isinstance(obj, bool):
-    return 'B'
+    return "B"
   if isinstance(obj, str):
-    return 'str'
-  raise NotImplementedError(f'could not compute type name for {obj}: {type(obj)}')
+    return "str"
+  raise NotImplementedError(
+      f"could not compute type name for {obj}: {type(obj)}"
+  )
+
 
 def get_triton_python_ir(aval):
   return get_triton_type(aval)
@@ -108,25 +110,31 @@ Grid = Union[Tuple[int], Tuple[int, int], Tuple[int, int, int]]
 GridOrLambda = Union[Grid, Callable[[Any], Grid]]
 
 triton_kernel_call_p = jax.core.Primitive('triton_kernel_call')
-triton_kernel_call_p.multiple_results =True
+triton_kernel_call_p.multiple_results = True
 
 triton_kernel_call_p.def_impl(
     functools.partial(xla.apply_primitive, triton_kernel_call_p))
 
 @triton_kernel_call_p.def_abstract_eval
 def triton_kernel_call_abstract_eval(*_, out_shapes, **__):
-  return [core.ShapedArray(out_shape.shape, out_shape.dtype) for out_shape in out_shapes]
+  return [
+      core.ShapedArray(out_shape.shape, out_shape.dtype)
+      for out_shape in out_shapes
+  ]
+
 
 def aval_to_layout(aval):
-  arange = np.arange(aval.ndim, dtype='int64')[::-1].copy()
+  arange = np.arange(aval.ndim, dtype="int64")[::-1].copy()
   return ir.DenseIntElementsAttr.get(arange, type=ir.IndexType.get())
+
 
 def avals_to_layouts(avals):
   return ir.ArrayAttr.get([aval_to_layout(a) for a in avals])
 
+
 def compile_triton_func(
     avals_in, avals_out, triton_func, num_warps, num_stages, metaparams):
-  metadata = {triton_func.arg_names.index(k) : v for k, v in metaparams.items()}
+  metadata = {triton_func.arg_names.index(k): v for k, v in metaparams.items()}
   all_args = [*avals_in, *avals_out]
   signature = {i: get_triton_python_ir(a) for i, a in enumerate(all_args)}
   # TODO(sharadmv,zhangqiaorjc): handle differently aligned pointers
@@ -175,10 +183,20 @@ def emit_triton_kernel_call(
   )
 
 
-def triton_kernel_call_lowering(ctx, *args, kernel_name, call_name, asm, shared_mem,
-                                out_shapes, grid: GridOrLambda, num_warps: int,
-                                dump_binary_path: Optional[str],
-                                input_output_aliases: Tuple[Tuple[int, int], ...], **metaparams):
+def triton_kernel_call_lowering(
+    ctx,
+    *args,
+    kernel_name,
+    call_name,
+    asm,
+    shared_mem,
+    out_shapes,
+    grid: GridOrLambda,
+    num_warps: int,
+    dump_binary_path: Optional[str],
+    input_output_aliases: Tuple[Tuple[int, int], ...],
+    **metaparams,
+):
   if jaxlib.version.__version_info__ < (0, 3, 22) and input_output_aliases:
     raise NotImplementedError(
         "`input_output_aliases` only supported on `jaxlib>=0.3.22")
@@ -197,13 +215,16 @@ def triton_kernel_call_lowering(ctx, *args, kernel_name, call_name, asm, shared_
       metaparams=metaparams,
   )
   ctx.module_context.add_keepalive(executable)
-  output_operand_aliases = ir.ArrayAttr.get([
+  output_operand_aliases = ir.ArrayAttr.get(
+      [
           mhlo.OutputOperandAlias.get(
               output_tuple_indices=[output],
               operand_index=input,
-              operand_tuple_indices=[])
+              operand_tuple_indices=[],
+          )
           for input, output in input_output_aliases
-      ])
+      ]
+  )
   out = mhlo.CustomCallOp(
       [out_type],
       args,
@@ -219,6 +240,8 @@ def triton_kernel_call_lowering(ctx, *args, kernel_name, call_name, asm, shared_
   results = [mhlo.GetTupleElementOp(out, mlir.i32_attr(i)).result
              for i in range(len(out_shapes))]
   return results
+
+
 mlir.register_lowering(triton_kernel_call_p, triton_kernel_call_lowering)
 
 class Asm:
@@ -320,32 +343,46 @@ def triton_call(*args: Array, kernel: triton.JITFunction,
   ```
   """
   if not CAN_USE_TRITON:
-    raise ValueError("`triton_call` is only available when `triton` is installed.")
+    raise ValueError(
+        "`triton_call` is only available when `triton` is installed."
+    )
   xc.register_custom_call_target(
-    call_name, triton_kernel_call_lib.get_custom_call(), platform="CUDA")
+      call_name, triton_kernel_call_lib.get_custom_call(), platform="CUDA"
+  )
   if isinstance(grid, int):
     grid = (grid,)
   out_shape = tree_util.tree_map(
       lambda a: jax.ShapeDtypeStruct(a.shape, a.dtype), out_shape)
   flat_args, in_tree = tree_util.tree_flatten(args)
   del in_tree
-  # TODO(sharadmv): check that in_tree is flat (no Pytrees allowed in triton_call)
+  # TODO(sharadmv): check in_tree is flat (no Pytrees allowed in triton_call)
   flat_out_shapes, out_tree = tree_util.tree_flatten(out_shape)
   avals_in = [core.raise_to_shaped(core.get_aval(a)) for a in flat_args]
   avals_out = [core.ShapedArray(a.shape, a.dtype) for a in flat_out_shapes]
   kernel_name, asm_map, shared_mem = compile_triton_func(
-    avals_in, avals_out, kernel, num_warps, num_stages, metaparams)
+      avals_in, avals_out, kernel, num_warps, num_stages, metaparams
+  )
   asm = Asm(asm_map)
-  out_flat = triton_kernel_call_p.bind(*flat_args, kernel_name=kernel_name,
-      call_name=call_name, asm=asm,
-      shared_mem=shared_mem, out_shapes=tuple(flat_out_shapes),
-      grid=grid, num_warps=num_warps, num_stages=num_stages,
+  out_flat = triton_kernel_call_p.bind(
+      *flat_args,
+      kernel_name=kernel_name,
+      call_name=call_name,
+      asm=asm,
+      shared_mem=shared_mem,
+      out_shapes=tuple(flat_out_shapes),
+      grid=grid,
+      num_warps=num_warps,
+      num_stages=num_stages,
       dump_binary_path=dump_binary_path,
-     input_output_aliases=tuple(input_output_aliases.items()), **metaparams)
+      input_output_aliases=tuple(input_output_aliases.items()),
+      **metaparams,
+  )
   return tree_util.tree_unflatten(out_tree, out_flat)
+
 
 def cdiv(a, b):
   return triton.cdiv(a, b)
+
 
 def strides_from_shape(shape: Tuple[int]) -> Tuple[int]:
   size = np.prod(shape)
@@ -354,6 +391,7 @@ def strides_from_shape(shape: Tuple[int]) -> Tuple[int]:
     size = size // s
     strides.append(int(size))
   return tuple(strides)
+
 
 def next_power_of_2(x: int) -> int:
   if x == 0:
