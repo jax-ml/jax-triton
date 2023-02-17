081d4b17210830876885771f37175ee48f405956
Qiao Zhang <zhangqiaorjc@google.com>
2023-02-17 03:57:32 +0000

    Add experimental fused layer norm backward pass.
    Co-authored-by: Sharad Vikram <sharadmv@google.com>

--- examples/pallas/layer_norm.py
+++ examples/pallas/layer_norm.py
@@ -12,115 +12,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import functools
 import timeit
 
-from typing import Optional
-
 import jax
-from jax import lax
 import jax.numpy as jnp
-from jax._src.lax.control_flow.for_loop import for_loop
 import numpy as np
 
-import jax_triton as jt
-from jax_triton import pallas as pl
-
-def layer_norm_forward_kernel(
-    x_ref, weight_ref, bias_ref, # Input arrays
-    o_ref, mean_ref=None, rstd_ref=None, # Output arrays
-    *, eps: float, block_size: int):
-  row_idx = pl.program_id(axis=0)
-
-  def mean_body(i, acc_ref):
-    col_idx = i * block_size + jnp.arange(block_size)
-    mask = col_idx < x_ref.shape[1]
-    a = pl.load(x_ref, (row_idx, col_idx), mask=mask, other=0.,
-                eviction_policy="evict_last").astype(jnp.float32)
-    acc_ref[:] += a
-  mean = for_loop(jt.cdiv(x_ref.shape[1], block_size), mean_body,
-                  jnp.zeros(block_size)).mean(axis=0)
-
-  def var_body(i, acc_ref):
-    col_idx = i * block_size + jnp.arange(block_size)
-    mask = col_idx < x_ref.shape[1]
-    a = pl.load(x_ref, (row_idx, col_idx), mask=mask, other=0.,
-                eviction_policy="evict_last").astype(jnp.float32)
-    a = jnp.where(mask, a - mean, 0.)
-    acc_ref[:] += a * a
-  var = for_loop(jt.cdiv(x_ref.shape[1], block_size), var_body,
-                 jnp.zeros(block_size)).mean()
-  rstd = 1 / jnp.sqrt(var + eps)
-  if mean_ref is not None:
-    mean_ref[row_idx] = mean
-  if rstd_ref is not None:
-    rstd_ref[row_idx] = rstd
-
-  def body(i, _):
-    col_idx = i * block_size + jnp.arange(block_size)
-    mask = col_idx < x_ref.shape[1]
-    weight = pl.load(weight_ref, (col_idx,), mask=mask)
-    bias = pl.load(bias_ref, (col_idx,), mask=mask)
-    x = pl.load(x_ref, (row_idx, col_idx), mask=mask, other=0.,
-                eviction_policy="evict_first").astype(jnp.float32)
-    out = (x - mean) * rstd * weight + bias
-    pl.store(o_ref, (row_idx, col_idx), out.astype(o_ref.dtype), mask=mask)
-  for_loop(jt.cdiv(x_ref.shape[1], block_size), body, ())
-
-
-@functools.partial(jax.jit, static_argnames=["num_warps", "num_stages",
-                                             "num_stages", "eps", "interpret"])
-def layer_norm(
-    x, weight, bias,
-    num_warps: Optional[int] = None,
-    num_stages: Optional[int] = 3,
-    eps: float = 1e-5,
-    interpret: bool = False):
-  m, n = x.shape
-  # Triton heuristics
-  # Less than 64KB per feature: enqueue fused kernel
-  max_fused_size = 65536 // x.dtype.itemsize
-  block_size = min(max_fused_size, jt.next_power_of_2(n))
-  block_size = min(max(block_size, 128), 4096)
-  num_warps = min(max(block_size // 256, 1), 8)
-
-  grid = m  # one thread per row
-
-  kernel = functools.partial(layer_norm_forward_kernel, eps=eps,
-                             block_size=block_size)
-  out_shape = jax.ShapeDtypeStruct(shape=x.shape, dtype=x.dtype)
-  return pl.pallas_call(kernel, num_warps=num_warps, num_stages=num_stages,
-                        grid=grid, out_shape=out_shape, debug=False,
-                        interpret=interpret)(x, weight, bias)
-
-
-@functools.partial(jax.jit, static_argnames=["eps"])
-def layer_norm_reference(x, weight, bias, *, eps: float = 1e-5):
-  mean = jnp.mean(x, axis=1)
-  mean2 = jnp.mean(jnp.square(x), axis=1)
-  var = jnp.maximum(0., mean2 - jnp.square(mean))
-  y = x - mean[:, None]
-  mul = lax.rsqrt(var + eps)
-  return y * mul[:, None] * weight[None] + bias[None]
-
+from jax_triton.pallas.ops import layer_norm
 
 if __name__ == "__main__":
   dtype = jnp.float32
-  m, n = 4096, 4096
+  b, m, n = 32, 4096, 8196
 
   weight_key, bias_key, x_key = jax.random.split(jax.random.PRNGKey(0), 3)
   weight = jax.random.normal(weight_key, (n,), dtype=dtype)
   bias = jax.random.normal(bias_key, (n,), dtype=dtype)
-  x = jax.random.normal(x_key, (m, n), dtype=dtype)
+  x = jax.random.normal(x_key, (b, m, n), dtype=dtype)
 
-  out = layer_norm(x, weight, bias)
-  out_ref = layer_norm_reference(x, weight, bias)
-  np.testing.assert_allclose(out, out_ref, atol=0.03, rtol=0.03)
+  out = layer_norm.layer_norm(x, weight, bias, interpret=True)
+  out_ref = layer_norm.layer_norm_reference(x, weight, bias)
+  np.testing.assert_allclose(out, out_ref, rtol=1e-6, atol=1e-6)
+
+  def f(x, w, b):
+      return layer_norm.layer_norm(x, w, b).sum()
+
+  def f_ref(x, w, b):
+      return layer_norm.layer_norm_reference(x, w, b).sum()
+
+  f_grad = jax.jit(jax.grad(f, argnums=(0, 1, 2)))
+  f_ref_grad = jax.jit(jax.grad(f_ref, argnums=(0, 1, 2)))
+
+  dx, dw, db = f_grad(x, weight, bias)
+  dx_ref, dw_ref, db_ref = f_ref_grad(x, weight, bias)
+  np.testing.assert_allclose(dx, dx_ref, rtol=1e-6, atol=1e-6)
+  np.testing.assert_allclose(dw, dw_ref, rtol=1e-2, atol=1e-2)
+  np.testing.assert_allclose(db, db_ref, rtol=1e-2, atol=1e-2)
 
   n_trials = 1000
-  duration = timeit.timeit(lambda: layer_norm(x, weight, bias).block_until_ready(),
+  duration = timeit.timeit(lambda: layer_norm.layer_norm(x, weight, bias).block_until_ready(),
                            number=n_trials)
   print(f"Fused Layer Norm: {duration / n_trials * 1000:.2f}ms")
-  duration = timeit.timeit(lambda: layer_norm_reference(x, weight, bias).block_until_ready(),
-                           number=n_trials)
+  duration = timeit.timeit(lambda: layer_norm.layer_norm_reference(x, weight, bias).block_until_ready(),
+                          number=n_trials)
   print(f"Reference Layer Norm: {duration / n_trials * 1000:.2f}ms")
+  duration = timeit.timeit(lambda: jax.block_until_ready(f_grad(x, weight, bias)),
+                          number=n_trials)
+  print(f"Fused Layer Norm Gradient: {duration / n_trials * 1000:.2f}ms")
+  duration = timeit.timeit(lambda: jax.block_until_ready(f_ref_grad(x, weight, bias)),
+                          number=n_trials)
+  print(f"Reference Layer Norm Gradient: {duration / n_trials * 1000:.2f}ms")
--- tests/pallas_test.py
+++ tests/pallas_test.py
@@ -35,6 +35,7 @@ import jax_triton as jt
 from jax_triton import pallas as pl
 from jax_triton.pallas.pallas_call import _initial_style_open_jaxpr
 from jax_triton.pallas.ops import attention
+from jax_triton.pallas.ops import layer_norm
 try:
   from jax_triton.pallas.triton_ir_lowering import compile_jaxpr
 except ModuleNotFoundError:
@@ -864,6 +865,52 @@ class FusedAttentionTest(parameterized.TestCase):
     np.testing.assert_allclose(dq, dq_ref, atol=0.05)
     np.testing.assert_allclose(dk, dk_ref, atol=0.05)
     np.testing.assert_allclose(dv, dv_ref, atol=0.05)
+
+
+class FusedLayerNormTest(parameterized.TestCase):
+
+  @parameterized.parameters(*[
+    (1, 384, 192),
+    (2, 384, 192),
+  ])
+  def test_fused_layernorm_fwd(self, batch_size, seq_len, embed_dim):
+    if torch is not None and torch.cuda.get_device_capability() < (7, 0):
+      raise unittest.SkipTest(
+          "Fused layernorm only works on GPUs with capability >= sm70")
+    k1, k2, k3 = random.split(random.PRNGKey(0), 3)
+    x = random.normal(k1, (batch_size, seq_len, embed_dim), dtype=jnp.float32)
+    w = jax.random.normal(k2, (embed_dim,), dtype=jnp.float32)
+    b = jax.random.normal(k3, (embed_dim,), dtype=jnp.float32)
+
+    o = layer_norm.layer_norm(x, w, b)
+    o_ref = layer_norm.layer_norm_reference(x, w, b)
+    np.testing.assert_allclose(o, o_ref, atol=1e-5)
+
+  @parameterized.parameters(*[
+    (1, 384, 192),
+    (2, 384, 192),
+  ])
+  def test_fused_layernorm_bwd(self, batch_size, seq_len, embed_dim):
+    if torch is not None and torch.cuda.get_device_capability() < (7, 0):
+      raise unittest.SkipTest(
+          "Fused layernorm only works on GPUs with capability >= sm70")
+    k1, k2, k3 = random.split(random.PRNGKey(0), 3)
+    x = random.normal(k1, (batch_size, seq_len, embed_dim), dtype=jnp.float32)
+    w = jax.random.normal(k2, (embed_dim,), dtype=jnp.float32)
+    b = jax.random.normal(k3, (embed_dim,), dtype=jnp.float32)
+
+    def f(x, w, b):
+      return layer_norm.layer_norm(x, w, b).sum()
+
+    def f_ref(x, w, b):
+      return layer_norm.layer_norm_reference(x, w, b).sum()
+
+    dx, dw, db = jax.grad(f, argnums=(0, 1, 2))(x, w, b)
+    dx_ref, dw_ref, db_ref = jax.grad(f_ref, argnums=(0, 1, 2))(x, w, b)
+    np.testing.assert_allclose(dx, dx_ref, rtol=1e-6, atol=1e-6)
+    np.testing.assert_allclose(dw, dw_ref, rtol=1e-2, atol=1e-2)
+    np.testing.assert_allclose(db, db_ref, rtol=1e-2, atol=1e-2)
+
 
 if __name__ == "__main__":
   absltest.main()
