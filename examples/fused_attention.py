7b6761870fe14ce31004ca7517d30f6224414a31
Sharad Vikram <sharad.vikram@gmail.com>
2023-03-02 00:04:41 +0000

    Fix attention example

--- examples/fused_attention.py
+++ examples/fused_attention.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 """Flash attention example."""
+import functools
 import jax
 from jax import random
 import jax.numpy as jnp
@@ -28,70 +29,73 @@ def _strides(shape):
     size = size // s
     yield int(size)
 
+HACK_CONSTANT = 0
 
 @triton.jit
 def fused_attention_kernel(
-    q_ptr,
-    k_ptr,
-    v_ptr,
-    tmp_ptr,
-    l_ptr,
-    m_ptr,  # NOTE: tmp_ptr is a scratchpad buffer to workaround a compiler bug
-    out_ptr,
-    stride_qz: tl.constexpr,  # pylint: disable=unused-argument
-    stride_qh: tl.constexpr,
-    stride_qm: tl.constexpr,
-    stride_qk: tl.constexpr,
-    stride_kz: tl.constexpr,  # pylint: disable=unused-argument
-    stride_kh: tl.constexpr,  # pylint: disable=unused-argument
-    stride_kk: tl.constexpr,
-    stride_kn: tl.constexpr,
-    stride_vz: tl.constexpr,  # pylint: disable=unused-argument
-    stride_vh: tl.constexpr,  # pylint: disable=unused-argument
-    stride_vk: tl.constexpr,
-    stride_vn: tl.constexpr,  # pylint: disable=unused-argument
-    stride_oz: tl.constexpr,  # pylint: disable=unused-argument
-    stride_oh: tl.constexpr,
-    stride_om: tl.constexpr,
-    stride_on: tl.constexpr,
-    z: tl.constexpr,  # pylint: disable=unused-argument
-    h: tl.constexpr,  # pylint: disable=unused-argument
-    n_ctx: tl.constexpr,
-    block_m: tl.constexpr,
-    block_dmodel: tl.constexpr,
-    block_n: tl.constexpr,
+    Q,
+    K,
+    V,
+    stride_qz,  # pylint: disable=unused-argument
+    stride_qh,
+    stride_qm,
+    stride_qk,
+    stride_kz,  # pylint: disable=unused-argument
+    stride_kh,  # pylint: disable=unused-argument
+    stride_kn,
+    stride_kk,
+    stride_vz,  # pylint: disable=unused-argument
+    stride_vh,  # pylint: disable=unused-argument
+    stride_vk,
+    stride_vn,  # pylint: disable=unused-argument
+    stride_oz,  # pylint: disable=unused-argument
+    stride_oh,
+    stride_om,
+    stride_on,
+    Z,  # pylint: disable=unused-argument
+    H,  # pylint: disable=unused-argument
+    N_CTX,
+    TMP,
+    L,
+    M,  # NOTE: tmp_ptr is a scratchpad buffer to workaround a compiler bug
+    Out,
+    BLOCK_M: tl.constexpr,
+    BLOCK_DMODEL: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    sm_scale: tl.constexpr,
 ):
   """Flash attention kernel."""
-  start_qm = tl.program_id(0)
+  start_m = tl.program_id(0)
   off_hz = tl.program_id(1)
   # initialize offsets
-  offs_m = start_qm * block_m + tl.arange(0, block_m)
-  offs_n = tl.arange(0, block_n)
-  offs_d = tl.arange(0, block_dmodel)
-  off_q = off_hz * stride_qh + offs_m[:, None] * stride_qm + offs_d[
-      None, :] * stride_qk
-  off_k = off_hz * stride_qh + offs_n[
-      None, :] * stride_kn + offs_d[:, None] * stride_kk
-  off_v = off_hz * stride_qh + offs_n[:, None] * stride_qm + offs_d[
-      None, :] * stride_qk
-  # Initialize pointers to q_ptr, k_ptr, v_ptr
-  q_ptrs = q_ptr + off_q
-  k_ptrs = k_ptr + off_k
-  v_ptrs = v_ptr + off_v
+  offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+  offs_n = tl.arange(0, BLOCK_N)
+  offs_d = tl.arange(0, BLOCK_DMODEL)
+  off_q = off_hz * stride_qh + offs_m[:, None] * stride_qm + offs_d[None, :] * stride_qk
+  off_k = off_hz * stride_qh + offs_n[:, None] * stride_kn + offs_d[None, :] * stride_kk
+  off_v = off_hz * stride_qh + offs_n[:, None] * stride_qm + offs_d[None, :] * stride_qk
+  # Initialize pointers to Q, K, V
+  q_ptrs = Q + off_q
+  k_ptrs = K + off_k
+  v_ptrs = V + off_v
   # initialize pointer to m and l
-  t_ptrs = tmp_ptr + off_hz * n_ctx + offs_m
-
-  acc = tl.zeros([block_m, block_dmodel], dtype=tl.float32)
-  m_i = tl.zeros([block_m], dtype=tl.float32) - float("inf")
-  l_i = tl.zeros([block_m], dtype=tl.float32)
-
+  t_ptrs = TMP + off_hz * N_CTX + offs_m
+  m_i = tl.zeros([BLOCK_M], dtype=tl.float32) - float("inf")
+  l_i = tl.zeros([BLOCK_M], dtype=tl.float32)
+  acc = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
+  # load q: it will stay in SRAM throughout
   q = tl.load(q_ptrs)
-  for start_n in range(0, start_qm + 1):
+  # loop over k, v and update accumulator
+  for start_n in range(0, N_CTX + HACK_CONSTANT, BLOCK_N):
+  # for start_n in range(0, (start_m + 1) * BLOCK_M, BLOCK_N):
+    start_n = tl.multiple_of(start_n, BLOCK_N)
     # -- compute qk ----
-    k = tl.load(k_ptrs)
-    qk = tl.dot(q, k)
-    qk += tl.where(offs_m[:, None] >= (start_n * block_n + offs_n[None, :]), 0,
-                   float("-inf"))
+    k = tl.load(k_ptrs + start_n * stride_kn)
+    qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+    qk += tl.dot(q, k, trans_b=True)
+    qk = qk * sm_scale
+    qk = qk.to(Q.dtype.element_ty)
+    qk = qk.to(tl.float32)
     # -- compute m_ij, p, l_ij
     m_ij = tl.max(qk, 1)
     p = tl.exp(qk - m_ij[:, None])
@@ -105,35 +109,31 @@ def fused_attention_kernel(
     # scale p
     p_scale = beta / l_i_new
     p = p * p_scale[:, None]
-    p = p.to(tl.float16)
     # scale acc
     acc_scale = l_i / l_i_new * alpha
     tl.store(t_ptrs, acc_scale)
     acc_scale = tl.load(t_ptrs)  # BUG: have to store and immediately load
     acc = acc * acc_scale[:, None]
     # update acc
-    v = tl.load(v_ptrs)
+    v = tl.load(v_ptrs + start_n * stride_vk)
+    p = p.to(tl.float16)
     acc += tl.dot(p, v)
-    k_ptrs += block_n * stride_kn
-    v_ptrs += block_n * stride_vk
-    # r_ptrs += block_n
+    # update m_i and l_i
     l_i = l_i_new
     m_i = m_i_new
-
-  start_qm = tl.program_id(0)
-  offs_m = start_qm * block_m + tl.arange(0, block_m)
+  # rematerialize offsets to save registers
+  start_m = tl.program_id(0)
+  offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
   # write back l and m
-  l_ptrs = l_ptr + off_hz * n_ctx + offs_m
-  m_ptrs = m_ptr + off_hz * n_ctx + offs_m
+  l_ptrs = L + off_hz * N_CTX + offs_m
+  m_ptrs = M + off_hz * N_CTX + offs_m
   tl.store(l_ptrs, l_i)
   tl.store(m_ptrs, m_i)
   # initialize pointers to output
-  offs_n = tl.arange(0, block_dmodel)
-  off_out = off_hz * stride_oh + offs_m[:, None] * stride_om + offs_n[
-      None, :] * stride_on
-  out_ptrs = out_ptr + off_out
+  offs_n = tl.arange(0, BLOCK_DMODEL)
+  off_o = off_hz * stride_oh + offs_m[:, None] * stride_om + offs_n[None, :] * stride_on
+  out_ptrs = Out + off_o
   tl.store(out_ptrs, acc)
-
 
 def fused_attention(q: jnp.ndarray, k: jnp.ndarray,
                     v: jnp.ndarray) -> jnp.ndarray:
@@ -149,40 +149,25 @@ def fused_attention(q: jnp.ndarray, k: jnp.ndarray,
           shape=(q.shape[0] * q.shape[1], q.shape[2]), dtype=q.dtype),
       jax.ShapeDtypeStruct(shape=q.shape, dtype=q.dtype)
   ]
-  stride_qz, stride_qh, stride_qm, stride_qk = _strides(q.shape)
-  stride_kz, stride_kh, stride_kk, stride_kn = _strides(k.shape)
-  stride_vz, stride_vh, stride_vk, stride_vn = _strides(v.shape)
-  stride_oz, stride_oh, stride_om, stride_on = _strides(out_shape[-1].shape)
 
   metaparams = dict(
-      block_m=block_size,
-      block_n=block_size,
-      block_dmodel=64,
-      stride_qz=stride_qz,
-      stride_qh=stride_qh,
-      stride_qm=stride_qm,
-      stride_qk=stride_qk,
-      stride_kz=stride_kz,
-      stride_kh=stride_kh,
-      stride_kk=stride_kk,
-      stride_kn=stride_kn,
-      stride_vz=stride_vz,
-      stride_vh=stride_vh,
-      stride_vk=stride_vk,
-      stride_vn=stride_vn,
-      stride_oz=stride_oz,
-      stride_oh=stride_oh,
-      stride_om=stride_om,
-      stride_on=stride_on,
-      z=q.shape[0],
-      h=q.shape[0],
-      n_ctx=q.shape[0],
+      BLOCK_M=block_size,
+      BLOCK_N=block_size,
+      BLOCK_DMODEL=64,
+      sm_scale=1.,
       num_warps=4,
       num_stages=1)
   _, _, _, output = jt.triton_call(
       q,
       k,
       v,
+      *jt.strides_from_shape(q.shape),
+      *jt.strides_from_shape(k.shape),
+      *jt.strides_from_shape(v.shape),
+      *jt.strides_from_shape(q.shape),
+      q.shape[0],
+      q.shape[1],
+      q.shape[2],
       kernel=fused_attention_kernel,
       out_shape=out_shape,
       grid=grid,
@@ -190,10 +175,18 @@ def fused_attention(q: jnp.ndarray, k: jnp.ndarray,
   return output
 
 
+@functools.partial(jax.jit, static_argnames=['sm_scale'])
+def mha_reference(q, k, v, sm_scale=1.0):
+  logits = jnp.einsum('bhtc,bhsc->bhts', q, k).astype(jnp.float32)
+  weights = jax.nn.softmax(logits * sm_scale).astype(q.dtype)
+  return jnp.einsum('bhts,bhsc->bhtc', weights, v)
+
 if __name__ == "__main__":
   q_key, k_key, v_key = random.split(random.PRNGKey(0), 3)
-  q = random.normal(q_key, (2, 3, 1024, 64), dtype=jnp.float16)
-  k = random.normal(k_key, (2, 3, 64, 1024), dtype=jnp.float16)
-  v = random.normal(v_key, (2, 3, 1024, 64), dtype=jnp.float16)
-  print(fused_attention(q, k, v))
-  print(jax.jit(fused_attention)(q, k, v))
+  batch_size, num_heads, seq_len, head_dim = 2, 3, 1024, 64
+  q = random.normal(q_key, (batch_size, num_heads, seq_len, head_dim), dtype=jnp.float16)
+  k = random.normal(k_key, (batch_size, num_heads, seq_len, head_dim), dtype=jnp.float16)
+  v = random.normal(v_key, (batch_size, num_heads, seq_len, head_dim), dtype=jnp.float16)
+  o = fused_attention(q, k, v).block_until_ready()
+  o_ref = mha_reference(q, k, v).block_until_ready()
+  np.testing.assert_allclose(o, o_ref, rtol=0.05, atol=1e-2)
