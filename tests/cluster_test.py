c7fb6eb55781ffbff80cdf409fb81f10138b8844
Stephen Huan <stephen.huan@cgdct.moe>
2024-12-20 18:59:51 -0800

    Fix H100 skip in cluster tests

--- tests/cluster_test.py
+++ tests/cluster_test.py
@@ -74,7 +74,7 @@ class ClusterTest(parameterized.TestCase):
         _dummy_fn(jnp.empty((16,)))
 
   def test_cluster_not_available(self):
-    if 'h100' in jax.devices()[0].device_kind.lower():
+    if 'h100' not in jax.devices()[0].device_kind.lower():
       self.skipTest('Clusters available only on H100s.')
 
     my_triton_call = functools.partial(jt.triton_call, num_ctas=2)
