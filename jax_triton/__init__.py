2b1dda22571be0b82697ade1c1d08c4c467367f6
Stephen Huan <stephen.huan@cgdct.moe>
2024-12-21 03:29:23 -0800

    Add CPU backend

--- jax_triton/__init__.py
+++ jax_triton/__init__.py
@@ -24,7 +24,6 @@ __all__ = [
     "__version_info__",
 ]
 
-from jax._src.lib import gpu_triton
 from jax_triton import utils
 from jax_triton.triton_lib import triton_call
 from jax.experimental.pallas import cdiv
@@ -32,16 +31,3 @@ from jax.experimental.pallas import next_power_of_2
 from jax.experimental.pallas import strides_from_shape
 from jax_triton.version import __version__
 from jax_triton.version import __version_info__
-
-try:
-  get_compute_capability = gpu_triton.get_compute_capability
-  get_serialized_metadata = gpu_triton.get_serialized_metadata
-except AttributeError:
-  raise ImportError(
-      "jax-triton requires JAX to be installed with GPU support. The "
-      "installation page on the JAX documentation website includes "
-      "instructions for installing a supported version:\n"
-      "https://jax.readthedocs.io/en/latest/installation.html"
-  )
-else:
-  del gpu_triton  # Not part of the API.
